7aa75ec01f231c0b0cad3de90ae9aadc0ba8d3cc
thuonghvd0056 <thuonghvd00564@fpt.edu.vn>
2018-10-23 08:41:29 +0700

    lan1

---
+++ FakeNews/FakeNews/Model/NewsItem.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeNews.Model
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+
+        public string Category { get; set; }
+
+        public string Headline { get; set; }
+
+        public string Subhead { get; set; }
+
+        public string DateLine { get; set; }
+
+        public string Image { get; set; }
+    }
+
+    public class NewManager
+    {
+        public static void GetNews(string category, ObservableCollection<NewsItem> newsItems)
+        {
+            var allItem = getNewsItems();
+            var filteredNewsItems = allItem.Where(p => p.Category == category).ToList();
+            newsItems.Clear();
+            filteredNewsItems.ForEach(p => newsItems.Add(p));
+        }
+
+        private static List<NewsItem> getNewsItems()
+        {
+            var items = new List<NewsItem>();
+
+            items.Add(new NewsItem()
+            {
+                Id = 1,
+                Category = "Financial",
+                Headline = "Lorem",
+                Subhead = "Doro sit amet",
+                DateLine = "Nunc tristique nec",
+                Image = "Assets/Financial.png"
+            });
+
+            items.Add(new NewsItem()
+            {
+                Id = 2,
+                Category = "Financial",
+                Headline = "Etiam ac felis viverra",
+                Subhead = "vulputate nisl ac,aliquet nisi",
+                DateLine = "tortor porttitor, eu fermentun ante congue",
+                Image = "Assets/Financial2.jpg"
+            });
+
+            items.Add(new NewsItem()
+            {
+                Id = 3,
+                Category = "Financial",
+                Headline = "Integer sed turpits erat",
+                Subhead = "Sed quis hendreit lorem, quis interdunm dolor",
+                DateLine = "in viverra metus facilisis sed",
+                Image = "Assets/Financial3.png"
+            });
+
+            items.Add(new NewsItem()
+            {
+                Id = 4,
+                Category = "Financial",
+                Headline = "Proin sem neque",
+                Subhead = "aliquet quis ipsum tincidunt",
+                DateLine = "Integer eleifend",
+                Image = "Assets/Financial4.png"
+            });
+
+            items.Add(new NewsItem()
+            {
+                Id = 5,
+                Category = "Financial",
+                Headline = "Mauris dictum non leo vitae tempor",
+                Subhead = "In nisl tortor, enelifend sed ipsum eget",
+                DateLine = "Curabitur dictum auge vitae element ulrices",
+                Image = "Assets/Financial5.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 6,
+                Category = "Food",
+                Headline = "Lorem ipsum",
+                Subhead = "Doro sit amet",
+                DateLine = "Nunc tristique nec",
+                Image = "Assets/Food1.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 7,
+                Category = "Food",
+                Headline = "Etiam ac felis viverra",
+                Subhead = "vulputate nisl ac,aliquet nisi",
+                DateLine = "tortor porttitor, eu fermentun ante congue",
+                Image = "Assets/Food2.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 8,
+                Category = "Food",
+                Headline = "Integer sed turpits erat",
+                Subhead = "Sed quis hendreit lorem, quis interdunm dolor",
+                DateLine = "in viverra metus facilisis sed",
+                Image = "Assets/Food3.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 9,
+                Category = "Food",
+                Headline = "Proin sem neque",
+                Subhead = "aliquet quis ipsum tincidunt",
+                DateLine = "Integer eleifend",
+                Image = "Assets/Food4.png"
+            });
+            items.Add(new NewsItem()
+            {
+                Id = 10,
+                Category = "Food",
+                Headline = "Mauris dictum non leo vitae tempor",
+                Subhead = "In nisl tortor, enelifend sed ipsum eget",
+                DateLine = "Curabitur dictum auge vitae element ulrices",
+                Image = "Assets/Food5.png"
+            });
+
+
+            return items;
+        }
+
+    }
+}
---
+++ GridAndStackPaneDemo/GridAndStackPaneDemo/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GridAndStackPaneDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            BackButton.Visibility = Visibility.Collapsed;
+            MyFrame.Navigate(typeof(Drink));
+            TitleTextBlock.Text = "Drinks";
+            Drink.IsSelected = true;
+        }
+        private void FoodButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyFrame.CanGoBack)
+            {
+                MyFrame.GoBack();
+                Drink.IsSelected = true;
+            }
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Drink.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Collapsed;
+                MyFrame.Navigate(typeof(Drink));
+                TitleTextBlock.Text = "Drink";
+            }
+            else if (Food.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+                MyFrame.Navigate(typeof(Food));
+                TitleTextBlock.Text = "Food";
+            }
+            else if (Historical.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+                MyFrame.Navigate(typeof(Historical));
+                TitleTextBlock.Text = "Historical";
+            }
+            else if (Favorites.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+                MyFrame.Navigate(typeof(Favorites));
+                TitleTextBlock.Text = "Favorites";
+            }
+            else if (Send.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+                MyFrame.Navigate(typeof(Send));
+                TitleTextBlock.Text = "Send";
+            }
+            else if (New.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+                MyFrame.Navigate(typeof(New));
+                TitleTextBlock.Text = "News";
+            }
+        }
+    }
+}
